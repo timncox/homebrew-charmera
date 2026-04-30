2c8bef705b3ddaf965e61516bce30a2a1ef0ae3d
timncox <timcox@gmail.com>
2026-04-30 17:14:21 -0400

    Update charmera cask to v1.2.2

--- Casks/charmera.rb
+++ Casks/charmera.rb
@@ -1,6 +1,6 @@
 cask "charmera" do
-  version "1.2.1"
-  sha256 "c4effc4593f338b6a41d70c26e27077fd3e3637de6f30ea3f9e342554ebfb9e3"
+  version "1.2.2"
+  sha256 "7f8e925be79309dab2fa36542fa0cc3f87db643886fe9fad35adec09bcbb68f7"
 
   url "https://github.com/timncox/charmerapp/releases/download/v#{version}/Charmera.dmg"
   name "Charmera"
